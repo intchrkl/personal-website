eb84c48a7209a070a0514da954cf5b4c4bc8e3c8
intchrkl <intat.tochirakul@gmail.com>
2025-06-15 23:36:13 +0700

    Added 404 fallback

--- src/pages/redirects/RedirectPage.jsx
+++ src/pages/redirects/RedirectPage.jsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import NotFound from "../notFound/NotFound";
+
 
 const redirectMap = {
   github: "https://github.com/intchrkl",
@@ -8,13 +10,23 @@ const redirectMap = {
 
 export default function RedirectPage() {
   const { target } = useParams();
+  const [redirects, setRedirects] = useState(null);
 
   useEffect(() => {
-    const url = redirectMap[target];
-    if (url) {
-      window.location.replace(url);
+    fetch("/redirects.json")
+    .then(res => res.json())
+    .then(data => setRedirects(data));
+  }, []);
+
+  useEffect(() => {
+    if (redirects && redirects[target]) {
+      window.location.replace(redirects[target]);
     }
-  }, [target]);
+  }, [redirects, target]);
+
+  if (!redirects) return <h2>Loading...</h2>;
+
+  if (!(target in redirects)) return <NotFound />
 
   return <h2>Redirecting...</h2>;
 }
