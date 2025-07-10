28bceb065463f41c7e4e64f848728bdec78fae37
intchrkl <intat.tochirakul@gmail.com>
2025-07-10 23:20:33 +0700

    404 page redesign

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { FaHome } from "react-icons/fa";
 
 export default function NotFound() {
   return (
-    <div style={{ padding: "2rem", textAlign: "center" }}>
+    <div style={{ padding: "2rem", textAlign: "center", color: "white" }}>
       <h1>404 - Page Not Found</h1>
       <p>Woops! The page you're looking for doesn't exist.</p>
-      <a href="/">Go back home</a>
+      <a href="/" className="social-button" style={{ display: "inline-flex", alignItems: "center", gap: "0.5rem", marginTop: "1rem" }}>
+        <FaHome className="social-icon" />
+        Go back home
+      </a>
     </div>
   );
 }
